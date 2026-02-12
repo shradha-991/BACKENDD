c5cf5b7c1dbd64302ffb3daf6c3a9a9460c1c02a
shradha-991 <shradhagarg0909@gmail.com>
2026-02-12 15:15:05 +0530

    first server

---
+++ CLASS WORK BACKEND/server1/First_se.js
@@ -0,0 +1,21 @@
+const user = {
+  username: "Shradha",
+  password: "12345"
+};
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const method = req.method;
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = url.pathname;
+
+  if (method == "GET" && pathname == "/user") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(user));
+  } 
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
