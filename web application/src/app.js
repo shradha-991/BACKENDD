b9cde5887d398427d559508eb7e7385a32e67c62
shradha-991 <shradhagarg0909@gmail.com>
2026-03-31 11:03:57 +0530

    basic server setup in app.js

---
+++ web application/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello server');
+});
+
+app.listen(port, () => {  
+  console.log(`Server running on http://localhost:${port}`);
+});
